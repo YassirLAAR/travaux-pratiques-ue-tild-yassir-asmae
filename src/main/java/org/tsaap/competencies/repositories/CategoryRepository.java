451d456134fea2f8f320396151426deec0b23d0a
Yassir <yassir.laaroussi@univ-tlse3.fr>
2016-10-06 09:14:14 +0200

    Update N2 TP2

--- src/main/java/org/tsaap/competencies/repositories/CategoryRepository.java
+++ src/main/java/org/tsaap/competencies/repositories/CategoryRepository.java
@@ -1,20 +1,10 @@
 package org.tsaap.competencies.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-<<<<<<< HEAD
-import org.tsaap.competencies.Catalog;
 import org.tsaap.competencies.Category;
 
-/**
- * Catalog repository
- */
-public interface CategoryRepository extends CrudRepository<Category,Long> {
-=======
-import org.tsaap.competencies.Category;
 
 /**
  * Created by Armstrong on 9/30/2016.
  */
-public interface CategoryRepository  extends CrudRepository<Category,Long> {
->>>>>>> 213e0bd35429507fa27f8b2b1d32aedecf175c59
-}
+public interface CategoryRepository  extends CrudRepository<Category,Long> {}
