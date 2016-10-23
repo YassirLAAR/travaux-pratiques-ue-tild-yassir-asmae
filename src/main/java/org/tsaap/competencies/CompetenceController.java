e733875fd482cfaca22a2f174ef52c9fc05b5a4e
Yassir <yassir.laaroussi@univ-tlse3.fr>
2016-10-23 15:57:07 +0200

    Update N6 TP3: Web Service et test 100%

---
+++ src/main/java/org/tsaap/competencies/CompetenceController.java
@@ -0,0 +1,12 @@
+package org.tsaap.competencies;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Armstrong on 10/23/2016.
+ */
+@Controller
+public class CompetenceController {
+
+    private Ce
+}
